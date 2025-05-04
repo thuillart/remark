5bec9eb10db69694b9df0c3776cb02c730eaed9a
thuillart <thuillart@users.noreply.github.com>
2025-05-04 12:37:26 -0400

    Update .gitignore to reflect new Prisma client path, adjust VSCode settings to include .gitignore, and modify import paths in db.ts and schema.prisma for improved organization and consistency.

--- lib/prisma/db.ts
+++ lib/prisma/db.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "../../generated/prisma/client";
 
 import ws from "ws";
 neonConfig.webSocketConstructor = ws;
