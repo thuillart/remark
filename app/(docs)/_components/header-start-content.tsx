2140b714142ac14d25c0efff169d8aea33305cd3
thuillart <thuillart@users.noreply.github.com>
2025-05-06 23:44:14 -0400

    Refactor documentation links and SDK references by updating the header component to use the new button variants, and modifying the Node.js and Next.js documentation to reflect the new APP_NAME constant and updated SDK package names. Adjust links to point to the correct repositories for improved clarity.

--- app/(docs)/_components/header-start-content.tsx
+++ app/(docs)/_components/header-start-content.tsx
@@ -1,20 +1,22 @@
 import "server-only";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 
 export function HeaderStartContent() {
   return (
     <>
       <hr className="h-4 w-px bg-border max-sm:hidden" />
-
-      <Button
-        asChild
-        variant="link"
-        className="inline-flex font-normal text-muted-foreground hover:text-foreground hover:no-underline max-sm:hidden"
+      <Link
+        href="/docs"
+        className={buttonVariants({
+          variant: "link",
+          className:
+            "inline-flex font-normal text-muted-foreground hover:text-foreground hover:no-underline max-sm:hidden",
+        })}
       >
-        <Link href="/docs">Documentation</Link>
-      </Button>
+        Documentation
+      </Link>
     </>
   );
 }
