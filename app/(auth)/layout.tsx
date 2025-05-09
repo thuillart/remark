0798aacc247bb9638f1535464a2e91881f18890d
thuillart <thuillart@users.noreply.github.com>
2025-05-09 18:20:41 -0400

    Refactor AuthLayout component for improved readability by adjusting TypeScript syntax and reordering class names in the container div. Update CSS variable for muted foreground color to maintain consistency.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,11 +4,13 @@ import { Nav } from "@/auth/components/nav";
 
 export default function AuthLayout({
   children,
-}: { children: React.ReactNode }) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Nav />
-      <div className="!max-w-lg container">
+      <div className="container !max-w-lg">
         <div className="flex min-h-dvh items-center justify-center py-8">
           <main className="w-full pt-8">
             <div className="flex flex-col gap-4">{children}</div>
