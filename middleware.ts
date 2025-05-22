22266a0176b3c5e2486cb777ef1893f023a4f7f1
thuillart <thuillart@users.noreply.github.com>
2025-05-22 14:26:44 -0400

    Update middleware configuration for clarity and maintainability
    - Added comments to the middleware matcher configuration for better understanding.
    - Ensured consistent formatting by placing the matcher pattern on a new line.

--- middleware.ts
+++ middleware.ts
@@ -54,5 +54,8 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|docs/|terms|notice).*)"],
+  matcher: [
+    // Don't need middleware
+    "/((?!_next/static|_next/image|favicon.ico|docs/|terms|notice).*)",
+  ],
 };
