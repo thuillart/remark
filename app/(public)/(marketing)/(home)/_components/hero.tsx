31576d0b27244a69463d9a1735bf952aa98154b3
thuillart <thuillart@users.noreply.github.com>
2025-05-29 08:42:41 +0200

    Refactor Hero component layout for improved readability and structure
    - Adjusted the layout of the Hero component by modifying class order for better clarity and consistency.
    - Enhanced text alignment and spacing for improved visual hierarchy and user experience.

--- app/(public)/(marketing)/(home)/_components/hero.tsx
+++ app/(public)/(marketing)/(home)/_components/hero.tsx
@@ -12,15 +12,15 @@ export function Hero() {
   return (
     <section className="container">
       <div className="relative flex flex-col items-center gap-6 pt-14 pb-25">
-        <div className="-z-1 -translate-x-1/2 absolute bottom-0 left-1/2 size-full [mask:radial-gradient(ellipse_at_center_calc(100%_-_100px),var(--color-background),transparent_75%)]">
+        <div className="absolute bottom-0 left-1/2 -z-1 size-full -translate-x-1/2 [mask:radial-gradient(ellipse_at_center_calc(100%_-_100px),var(--color-background),transparent_75%)]">
           <HeroCanvas />
         </div>
         <div className="flex flex-col items-center gap-9">
-          <div className="mt-24 flex flex-col items-center gap-4 text-balance text-center">
-            <h1 className="font-semibold text-4xl/12 tracking-tighter md:text-5xl/14">
+          <div className="flex flex-col items-center gap-4 text-center text-balance md:mt-10">
+            <h1 className="text-4xl/12 font-semibold tracking-tighter md:text-5xl/14">
               Ship faster, build better
             </h1>
-            <p className="text-lg text-muted-foreground md:text-xl">
+            <p className="text-muted-foreground text-lg md:text-xl">
               Collect feedback, iterate and ship products people love.
             </p>
           </div>
